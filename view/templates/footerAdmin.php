e9915b19507f9cff8a8a94922e6e4b40b526aa89
Raecambra12 <alex.alvaradot@ug.edu.ec>
2024-01-21 17:04:16 -0500

    Webote

---
+++ view/templates/footerAdmin.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Título de tu Página</title>
+    <style>
+        .footerAdmin {
+            background-color:#979797;
+            color: black;
+            text-align: center;
+            padding: 20px;
+            position: relative;
+            bottom: 0;
+            width: 100%;
+            border-radius: 10px;
+        }
+
+        .footer-links {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+
+        .footer-links li {
+            display: inline;
+            padding: 0 20px;
+        }
+
+        .footer-links a {
+            color: #4c4c4c;
+            text-decoration: none;
+        }
+
+        .footer-links a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+
+
+
+    <footer class="footerAdmin">
+        <div>
+            <h3>Tienda De Juegos</h3>
+            <p>Guayaquil - Ecuador | Teléfono: +593 99844732</p>
+            <p>Correo electrónico: contacto@tiendaJuegos.com</p>
+        </div>
+        <ul class="footer-links">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Acerca de</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Política de Privacidad</a></li>
+        </ul>
+        <div>
+            <p>&copy; 2024 Alex Alvarado. Todos los derechos reservados.</p>
+        </div>
+    </footer>
+
+</body>
+</html>
---
+++ view/templates/headerAdmin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Tu Sitio Web</title>
+    <style>
+        .header {
+            background-color: #f1f1f1;
+            padding: 20px;
+            text-align: center;
+        }
+        .nav-bar {
+            overflow: hidden;
+            background-color: #333;
+        }
+        .nav-bar a {
+            float: left;
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+        .nav-bar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+    </style>
+</head>
+<body>
+
+<div class="header">
+    <h1>Mi Sitio Web</h1>
+    <p>Encabezado de mi sitio web</p>
+</div>
+
+<div class="nav-bar">
+    <a href="modulo1.php">Módulo 1</a>
+    <a href="modulo2.php">Módulo 2</a>
+    <a href="modulo3.php">Módulo 3</a>
+    <a href="modulo4.php">Módulo 4</a>
+</div>
+
+</body>
+</html>
